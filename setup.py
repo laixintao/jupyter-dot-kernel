50958db6e6f63b86620408d6a7e2c2e645b9e67e
laixintao <nanshi.lxt@alipay.com>
2019-08-09 17:52:33 +0800

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dot_kernel",
-    version="0.1.6",
+    version="0.1.7",
     url="https://github.com/laixintao/jupyter-dot-kernel",
     author="laixintao",
     author_email="laixintao1995@163.com",
