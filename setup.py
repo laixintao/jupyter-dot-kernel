9747cd90109d3bad08bf8b87fac6e525de2795fe
laixintao <nanshi.lxt@alipay.com>
2019-09-22 13:55:48 +0800

    bugfix: setup.py package

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email="laixintao1995@163.com",
     description="Writing dot language and render in jupyter.",
     packages=find_packages(),
-    package_data={'': '*.json'},
+    package_data={'': ['*.json']},
     install_requires=["graphviz", "jupyter"],
     scripts=['bin/install-dot-kernel']
 )
